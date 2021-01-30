0e2d7710c92cef813533489d3ab33d8ecff1425f
bqxtt <1486126243@qq.com>
2021-01-30 17:26:33 +0800

    submission status

--- pkg/common/constants/oj.go
+++ pkg/common/constants/oj.go
@@ -34,3 +34,28 @@ var (
 		StatusUrl:  "/status.php?user=%v&pid=%v",
 	}
 )
+
+type SubmissionStatus int32
+
+const (
+	PENDING SubmissionStatus = 0
+
+	SUBMIT_FAILED_TEMP SubmissionStatus = 1
+	SUBMIT_FAILED_PERM SubmissionStatus = 2
+
+	SUBMITTED SubmissionStatus = 3
+
+	QUEUEING  SubmissionStatus = 4
+	COMPILING SubmissionStatus = 5
+	JUDGING   SubmissionStatus = 6
+
+	AC           SubmissionStatus = 7
+	PE           SubmissionStatus = 8
+	WA           SubmissionStatus = 9
+	TLE          SubmissionStatus = 10
+	MLE          SubmissionStatus = 11
+	OLE          SubmissionStatus = 12
+	RE           SubmissionStatus = 13
+	CE           SubmissionStatus = 14
+	FAILED_OTHER SubmissionStatus = 15
+)
